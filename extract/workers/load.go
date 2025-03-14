6aa7fed591f0affa040f6cf6eb327721fd1022ea
juanvieira <juanvieira1@yahoo.com>
2025-03-14 19:58:33 -0300

    feat: add upload file to GCS fn

---
+++ extract/workers/load.go
@@ -0,0 +1,35 @@
+package workers
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/storage"
+)
+
+func UploadParquetOnGCS(parquetData[]byte, bucketName string, objectName string) error {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("storage.NewClient: %w", err)
+	}
+
+	defer client.Close()
+
+	bucket := client.Bucket(bucketName)
+	object := bucket.Object(objectName)
+	w := object.NewWriter(ctx)
+
+	if _, err := w.Write(parquetData); err != nil {
+		return fmt.Errorf("erro ao escrever no GCS: %w", err)
+	}
+
+	if err := w.Close(); err != nil {
+		return fmt.Errorf("erro ao fechar o writer do GCS: %w", err)
+	}
+
+	log.Printf("Arquivo Parquet salvo no GCS: gs://%s/%s", bucketName, objectName)
+	return nil
+}
