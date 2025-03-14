f59ba4123b50a6e310c5e36877799af646cfeb41
juanvieira <juanvieira1@yahoo.com>
2025-03-14 19:59:42 -0300

    feat: add main script

---
+++ extract/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"extract/constants"
+	"extract/internal"
+	"extract/requests"
+	"extract/workers"
+	"log"
+	"fmt"
+)
+
+func main()  {
+	constants.InitLogger()
+	
+	showTitle := "Black Mirror"
+	jsonData := requests.ExtractMoviesByTitle(showTitle)
+
+	parquetData, err := internal.JSONtoParquet(jsonData)
+	if err != nil {
+		log.Fatalf("Erro ao converter JSON para Parquet: %v", err)
+	}
+
+	bucketName := "streaming-data-for-ml"
+	objectName := fmt.Sprintf("data/%s.parquet", showTitle)
+	
+	if err := workers.UploadParquetOnGCS(parquetData, bucketName, objectName); err != nil {
+		log.Fatalf("Erro ao fazer upload para GCS: %v", err)
+	}
+}
